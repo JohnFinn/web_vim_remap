6f00d9819d6c4209d2bd65b04f957faaebcdfb22
JohnFinn <dz4tune@gmail.com>
2024-10-25 22:35:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Remaps hjkl to jkl; in vim mode on compiler explorer site.
+Thanks this [response](https://stackoverflow.com/questions/79099129/browser-extension-modifying-sites-javascript/79106723#79106723) on stackoverflow for helping me to implement it
---
+++ background.js
@@ -0,0 +1,38 @@
+(function () {
+  "use strict";
+
+  function onBR(e) {
+    var stream = browser.webRequest.filterResponseData(e.requestId),
+      dec = new TextDecoder("UTF-8"),
+      data = [];
+
+    stream.ondata = function (e) {
+      data.push(dec.decode(e.data, { stream: true }));
+    };
+    stream.onstop = function () {
+      var s = data.join(""),
+        enc = new TextEncoder(),
+        textFind = "t.prototype.enableVim=function(){",
+        textAdd =
+          "g.VimMode.Vim.noremap(';', 'l');" +
+          "g.VimMode.Vim.noremap('l', 'k');" +
+          "g.VimMode.Vim.noremap('k', 'j');" +
+          "g.VimMode.Vim.noremap('j', 'h');" +
+          "g.VimMode.Vim.noremap('h', ';');";
+
+      s = s.replace(textFind, "$&" + textAdd);
+      stream.write(enc.encode(s));
+      stream.close();
+    };
+    return {};
+  }
+
+  browser.webRequest.onBeforeRequest.addListener(
+    onBR,
+    {
+      types: ["script"],
+      urls: ["https://static.ce-cdn.net/main.v53.3050e299479d2cb0c9fa.js"],
+    },
+    ["blocking"],
+  );
+})();
