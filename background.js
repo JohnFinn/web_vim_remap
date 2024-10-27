31579b9d4afb95b53291053202b60a78a7c0b70f
JohnFinn <dz4tune@gmail.com>
2024-10-27 20:03:28 +0100

    refactoring to increase DRYness

--- background.js
+++ background.js
@@ -1,65 +1,57 @@
 (function () {
   "use strict";
 
-  function onBR(e) {
-    var stream = browser.webRequest.filterResponseData(e.requestId),
-      dec = new TextDecoder("UTF-8"),
-      data = [];
+  function makeTextPatcher(func) {
+    return (e) => {
+      var stream = browser.webRequest.filterResponseData(e.requestId),
+        dec = new TextDecoder("UTF-8"),
+        data = [];
 
-    stream.ondata = function (e) {
-      data.push(dec.decode(e.data, { stream: true }));
-    };
-    stream.onstop = function () {
-      var s = data.join(""),
-        enc = new TextEncoder(),
-        textFind = "t.prototype.enableVim=function(){",
-        textAdd =
-          "g.VimMode.Vim.noremap(';', 'l');" +
-          "g.VimMode.Vim.noremap('l', 'k');" +
-          "g.VimMode.Vim.noremap('k', 'j');" +
-          "g.VimMode.Vim.noremap('j', 'h');" +
-          "g.VimMode.Vim.noremap('h', ';');";
+      stream.ondata = function (e) {
+        data.push(dec.decode(e.data, { stream: true }));
+      };
+      stream.onstop = function () {
+        var s = data.join(""),
+          enc = new TextEncoder();
 
-      s = s.replace(textFind, "$&" + textAdd);
-      stream.write(enc.encode(s));
-      stream.close();
+        stream.write(enc.encode(func(s)));
+        stream.close();
+      };
+      return {};
     };
-    return {};
   }
 
-  function onBR2(e) {
-    var stream = browser.webRequest.filterResponseData(e.requestId),
-      dec = new TextDecoder("UTF-8"),
-      data = [];
+  var onBR = makeTextPatcher((s) => {
+    var textFind = "t.prototype.enableVim=function(){",
+      textAdd =
+        "g.VimMode.Vim.noremap(';', 'l');" +
+        "g.VimMode.Vim.noremap('l', 'k');" +
+        "g.VimMode.Vim.noremap('k', 'j');" +
+        "g.VimMode.Vim.noremap('j', 'h');" +
+        "g.VimMode.Vim.noremap('h', ';');";
 
-    stream.ondata = function (e) {
-      data.push(dec.decode(e.data, { stream: true }));
-    };
-    stream.onstop = function () {
-      var s = data.join(""),
-        enc = new TextEncoder(),
-        s = s
-          .replace(
-            '{keys:"h",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!1}}',
-            '{keys:"j",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!1}}',
-          )
-          .replace(
-            '{keys:"l",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!0}}',
-            '{keys:";",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!0}}',
-          )
-          .replace(
-            '{keys:"j",type:"motion",motion:"moveByLines",motionArgs:{forward:!0,linewise:!0}}',
-            '{keys:"k",type:"motion",motion:"moveByLines",motionArgs:{forward:!0,linewise:!0}}',
-          )
-          .replace(
-            '{keys:"k",type:"motion",motion:"moveByLines",motionArgs:{forward:!1,linewise:!0}}',
-            '{keys:"l",type:"motion",motion:"moveByLines",motionArgs:{forward:!1,linewise:!0}}',
-          );
-      stream.write(enc.encode(s));
-      stream.close();
-    };
-    return {};
-  }
+    return s.replace(textFind, "$&" + textAdd);
+  });
+
+  var onBR2 = makeTextPatcher((s) => {
+    return s
+      .replace(
+        '{keys:"h",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!1}}',
+        '{keys:"j",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!1}}',
+      )
+      .replace(
+        '{keys:"l",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!0}}',
+        '{keys:";",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!0}}',
+      )
+      .replace(
+        '{keys:"j",type:"motion",motion:"moveByLines",motionArgs:{forward:!0,linewise:!0}}',
+        '{keys:"k",type:"motion",motion:"moveByLines",motionArgs:{forward:!0,linewise:!0}}',
+      )
+      .replace(
+        '{keys:"k",type:"motion",motion:"moveByLines",motionArgs:{forward:!1,linewise:!0}}',
+        '{keys:"l",type:"motion",motion:"moveByLines",motionArgs:{forward:!1,linewise:!0}}',
+      );
+  });
 
   browser.webRequest.onBeforeRequest.addListener(
     onBR,
