36461362eff6b67bfc5ea6731cc232832ba3de10
JohnFinn <dz4tune@gmail.com>
2025-04-03 20:15:50 +0200

    some cleanup

--- proxy.py
+++ proxy.py
@@ -1,16 +1,9 @@
-import hashlib
-import re
 from mitmproxy import http
 
 
 def log(*a, **kw):
     with open("/tmp/log.txt", "at") as file:
         print(*a, **kw, file=file)
-
-
-def calculate_sha256(data: bytes) -> str:
-    """Compute SHA-256 hash of the given data."""
-    return hashlib.sha256(data).hexdigest()
 
 
 def matches_leetcode_js(url: str) -> bool:
@@ -21,8 +14,7 @@ def response(flow: http.HTTPFlow) -> None:
     """Log URL and SHA-256 hash of the response body."""
     if not matches_leetcode_js(flow.request.pretty_url):
         return
-    log("matched", flow.request.pretty_url)
-    log("replacing content")
+    log("matched", flow.request.pretty_url, "replacing content")
     flow.response.content = (
         flow.response.content.replace(
             b'{keys:"h",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!1}}',
